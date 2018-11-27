ab3f3380fa4e5c28c7c8f2ce0af429be663f1967
Nathan Blair <nateblairfilm@gmail.com>
2018-11-27 10:17:41 -0600

    component names are case sensitive

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -43,22 +43,22 @@
 						</div>
 						<div class="px-12">
 							<div v-if="showDropdowns" class="text-center w-full">
-								<circledropdown :model="findSettingByName('bg_page')[0]" :options="paletteOptions"></circledropdown>
+								<CircleDropdown :model="findSettingByName('bg_page')[0]" :options="paletteOptions"></CircleDropdown>
 							</div>
 							<header class="mb-4" v-bind:style="getStyle('font_h')">
-								<h1>Sample Heading <circledropdown v-if="showDropdowns" :model="findSettingByName('font_h')[0]" :options="paletteOptions" :settings="{ axis: 'y' }"></circledropdown></h1>
+								<h1>Sample Heading <CircleDropdown v-if="showDropdowns" :model="findSettingByName('font_h')[0]" :options="paletteOptions" :settings="{ axis: 'y' }"></CircleDropdown></h1>
 								<h2>Sample Subheading</h2>
 							</header>
 
 							<article class="my-4" v-bind:style="getStyle('font_p')">
 								<p class="my-4">
 									<div class="float-right">
-										<circledropdown v-if="showDropdowns" :model="findSettingByName('font_p')[0]" :options="paletteOptions" :settings="{ axis: 'y' }"></circledropdown>
+										<CircleDropdown v-if="showDropdowns" :model="findSettingByName('font_p')[0]" :options="paletteOptions" :settings="{ axis: 'y' }"></CircleDropdown>
 									</div>
-									Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor <a v-bind:style="getStyle('font_a')" href="#">incididunt</a> <circledropdown v-if="showDropdowns" :model="findSettingByName('font_a')[0]" :options="paletteOptions" :settings="{ axis: 'y' }"></circledropdown> ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse <a v-bind:style="getStyle('font_a')" href="#"> cillum dolore eu fugiat</a> nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+									Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor <a v-bind:style="getStyle('font_a')" href="#">incididunt</a> <CircleDropdown v-if="showDropdowns" :model="findSettingByName('font_a')[0]" :options="paletteOptions" :settings="{ axis: 'y' }"></CircleDropdown> ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse <a v-bind:style="getStyle('font_a')" href="#"> cillum dolore eu fugiat</a> nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 								</p>
 								<ul class="my-4" v-bind:style="getStyle('font_li')">
-									<li>List item 1 <circledropdown v-if="showDropdowns" :model="findSettingByName('font_li')[0]" :options="paletteOptions"></circledropdown></li>
+									<li>List item 1 <CircleDropdown v-if="showDropdowns" :model="findSettingByName('font_li')[0]" :options="paletteOptions"></CircleDropdown></li>
 									<li>List item 2</li>
 									<li>List item 3</li>
 									<li>List item 4</li>
@@ -82,7 +82,7 @@
 									</select>
 								</fieldset>
 								<fieldset class="py-2 w-full">
-									<button v-bind:style="[getStyle('bg_button'), getStyle('font_button')]" class="font-bold text-lg display-block px-3 py-2 my-4 border w-full sm:w-1/3 lg:w-32">Button</button> <circledropdown v-if="showDropdowns" :model="findSettingByName('font_button')[0]" :options="paletteOptions" :settings="{ axis: 'y' }"></circledropdown> <circledropdown v-if="showDropdowns" :model="findSettingByName('bg_button')[0]" :options="paletteOptions" :settings="{ axis: 'x' }"></circledropdown>
+									<button v-bind:style="[getStyle('bg_button'), getStyle('font_button')]" class="font-bold text-lg display-block px-3 py-2 my-4 border w-full sm:w-1/3 lg:w-32">Button</button> <CircleDropdown v-if="showDropdowns" :model="findSettingByName('font_button')[0]" :options="paletteOptions" :settings="{ axis: 'y' }"></CircleDropdown> <CircleDropdown v-if="showDropdowns" :model="findSettingByName('bg_button')[0]" :options="paletteOptions" :settings="{ axis: 'x' }"></CircleDropdown>
 								</fieldset>
 							</form>
 						</div>
